1c3cf688ba037e93d50c0cf854c7e8ebd1dd2017
CrazzyMCPE <crazzygamermcpe@gmail.com>
2016-05-24 21:44:47 -0700

    more stuff

---
+++ js/UI-Size.js
@@ -0,0 +1,91 @@
+/*UI Sizes Options*/
+/* Small UI panels*/
+	
+$(document).ready(function(){
+    $("h6").click(function(){
+        var div = $("h3");
+	
+        div.animate({height: '30%', opacity: '1.0', }, "slow");
+        div.animate({width: '60%', opacity: '1.0'}, "slow");
+    });
+	});
+	 $(document).ready(function(){
+    $("h6").click(function(){
+        var div = $("h5");
+	
+        div.animate({height: '30%', opacity: '1.0', }, "slow");
+        div.animate({width: '60%', opacity: '1.0'}, "slow");
+});
+});
+   $(document).ready(function(){
+    $("h6").click(function(){
+        var div = $("input");
+	
+        div.animate({height: '9%', opacity: '1.0', }, "slow");
+        div.animate({width: '20%', opacity: '1.0'}, "slow");
+   });
+	});
+$(document).ready(function(){
+    $("h6").click(function(){
+        var div = $("h6");
+	
+        div.animate({height: '10%', opacity: '1.0', }, "slow");
+        div.animate({width: '20%', opacity: '1.0'}, "slow"); 
+   });
+   });
+   $(document).ready(function(){
+    $("h6").click(function(){
+        var div = $("h4");
+	
+        div.animate({height: '10%', opacity: '1.0', }, "slow");
+        div.animate({width: '20%', opacity: '1.0'}, "slow");
+
+    });
+	});
+	
+	
+ /* Normal UI panels*/
+
+
+$(document).ready(function(){
+    $("h4").click(function(){
+        var div = $("h3");
+	
+        div.animate({height: '50%', opacity: '1.0'}, "slow");
+        div.animate({width: '80%', opacity: '1.0'}, "slow");
+     });
+});
+$(document).ready(function(){
+    $("h4").click(function(){
+        var div = $("h5");
+	
+        div.animate({height: '50%', opacity: '1.0', }, "slow");
+        div.animate({width: '80%', opacity: '1.0'}, "slow");
+});
+});
+$(document).ready(function(){
+    $("h4").click(function(){
+        var div = $("input");
+	
+        div.animate({height: '15%', opacity: '1.0', }, "slow");
+        div.animate({width: '22%', opacity: '1.0'}, "slow");
+    });
+	});
+	
+	$(document).ready(function(){
+    $("h4").click(function(){
+        var div = $("h6");
+	
+        div.animate({height: '15%', opacity: '1.0', }, "slow");
+        div.animate({width: '23%', opacity: '1.0'}, "slow");
+   });
+   });
+   $(document).ready(function(){
+    $("h4").click(function(){
+        var div = $("h4");
+	
+        div.animate({height: '10%', opacity: '1.0', }, "slow");
+        div.animate({width: '23%', opacity: '1.0'}, "slow");
+
+    });
+	});
--- js/splash.js
+++ js/splash.js
@@ -1,34 +1,70 @@
-var select=parseInt(Math.random()*5)+1;
+var select=parseInt(Math.random()*11)+1;
 
 		if(select==1){
 						document.write('<div class="tilt">');
-			document.write('<br> <br> <div id="splash1" class="pop">SmartPE WebSite!</div>');
+			document.write('<div id="splash" class="pop">SmartPE WebSite!</div>');
     document.write('</div>');
 }
 
 		 if(select==2){
 			document.write('<div class="tilt">');
-			document.write('<div id="splash1" class="pop">Baia...</div>');
+			document.write('<div id="splash" class="pop">Baia...</div>');
     document.write('</div>');
 }
 
 
 		 if(select==3){
 						document.write('<div class="tilt">');
-			document.write('<div id="splash1" class="pop">Soi el llolloz xD</div>');
+			document.write('<div id="splash" class="pop">Soi el llolloz xD</div>');
     document.write('</div>');
 }
 
 
 		 if(select==4){
 			document.write('<div class="tilt">'); 
-			document.write('<div id="splash1" class="pop">Allahu Akbar!!</div>');
+			document.write('<div id="splash" class="pop">Allahu Akbar!!</div>');
     document.write('</div>');
 }
 
 
 		 if(select==5){
 						document.write('<div class="tilt">');   
-			document.write('<div id="splash1" class="pop">Meh...</div>');
+			document.write('<div id="splash" class="pop">Meh...</div>');
+    document.write('</div>');
+}
+
+if(select==6){
+						document.write('<div class="tilt">');   
+			document.write('<div id="splash" class="pop">Cows are nice!!!</div>');
+    document.write('</div>');
+}
+
+if(select==7){
+			document.write('<div class="tilt">'); 
+			document.write('<div id="splash" class="pop">Kiss the sky...</div>');
+    document.write('</div>');
+}
+
+if(select==8){
+			document.write('<div class="tilt">'); 
+			document.write('<div id="splash" class="pop">Cows are nice!!!</div>');
+    document.write('</div>');
+}
+
+if(select==9){
+						document.write('<div class="tilt">');   
+			document.write('<div id="splash" class="pop">Flowers are better than grass</div>');
+    document.write('</div>');
+}
+
+if(select==10){
+			document.write('<div class="tilt">'); 
+			document.write('<div id="splash" class="pop">Crazzy!</div>');
+    document.write('</div>');
+}
+
+if(select==11){
+			document.write('<div class="tilt">'); 
+			document.write('<div id="splash" class="pop">Twitter love</div>');
     document.write('</div>');
 }
